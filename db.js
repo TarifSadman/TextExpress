f293a3bca4c5b6f2806ff2b760797d9f91e679b9
Tarif Sadman (Widerfunnel Marketing Inc) <v-tsadman@microsoft.com>
2025-07-16 18:42:28 +0600

    Express Init

--- db.js
+++ db.js
@@ -5,10 +5,21 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+      }
+);
+
+export { pool };
